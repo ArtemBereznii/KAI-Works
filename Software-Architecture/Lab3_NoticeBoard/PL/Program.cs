fd71e8deb578c6e7a2adc8ee17c025827566528c
ArtemBereznii <artem.berezniy8@gmail.com>
2026-05-27 13:10:11 +0300

    style: minimise comments

--- Software-Architecture/Lab3_NoticeBoard/PL/Program.cs
+++ Software-Architecture/Lab3_NoticeBoard/PL/Program.cs
@@ -12,7 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddEnvironmentVariables();
 
-// PostgreSQL connection string builder matching your reference
 var connectionString = $"Host={Env.GetString("DB_HOST", "localhost")};" +
                        $"Port={Env.GetString("DB_PORT", "5432")};" +
                        $"Database={Env.GetString("POSTGRES_DB", "NoticeBoardDb")};" +
@@ -24,30 +23,25 @@ builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
-        // Ensures Enums (like AdStatus.Active) show up as strings in Swagger/JSON instead of integers
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Injecting the layers we built
 builder.Services.AddDataAccess(builder.Configuration);
 builder.Services.AddBusinessLogic();
 
 var app = builder.Build();
 
-// Hooking up the custom exception handler
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
-// Enable Swagger in development
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Automatic EF Core Migrations on Startup
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
